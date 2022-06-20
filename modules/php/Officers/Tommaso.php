08e7c8c1138de9344208f4e8aa4cf443c4bc2844
vtoper <vincent.toper@gmail.com>
2022-06-21 00:06:17 +0200

    fix of available XO that should not

--- modules/php/Officers/Tommaso.php
+++ modules/php/Officers/Tommaso.php
@@ -12,4 +12,9 @@ class Tommaso extends \BRG\Models\Officer
       'Replace one of your 12 starting Engineers with the Architect at the beginning of the game. The Architect is a special Engineer. If you place the Architect in an action space requiring only one Engineer, you can immediately take another turn. If you place the Architect together with other Engineers in an action space requiring two or three Engineers, you do not activate this special ability.'
     );
   }
+
+  public function isAvailable()
+  {
+    return false;
+  }
 }
