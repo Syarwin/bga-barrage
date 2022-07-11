64f40f82d135e769fbac4ae573837f78603186eb
Syarwin <timothee.pecatta@ens-lyon.fr>
2022-07-11 15:11:10 +0200

    67745 - typo

--- modules/php/Officers/Viktor.php
+++ modules/php/Officers/Viktor.php
@@ -9,7 +9,7 @@ class Viktor extends \BRG\Models\Officer
     $this->id = \XO_VIKTOR;
     $this->name = clienttranslate('Viktor Fiesler');
     $this->description = clienttranslate(
-      'When you produce less than 4 Energy Units witha a production action, you produce 4 Energy Units instead. Multiply the total of Water Drops by the value of the Conduit that you are using: if the total is three or less you should consider the total to be 4. Then you must apply any bonus/malus of the action symbol and any bonus of your Company Board.'
+      'When you produce less than 4 Energy Units with a production action, you produce 4 Energy Units instead. Multiply the total of Water Drops by the value of the Conduit that you are using: if the total is three or less you should consider the total to be 4. Then you must apply any bonus/malus of the action symbol and any bonus of your Company Board.'
     );
   }
 }
